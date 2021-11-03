d46ef3e850cd65a2be2661b89758cf00635d4daf
steven <stevenmatton100@gmail.com>
2021-11-02 22:14:17 -0400

    first experience using timeOut function

---
+++ typewriter.js
@@ -0,0 +1,9 @@
+const sentence = "hello there from lighthouse labs";
+let counter = 0;
+for (const char of sentence) {
+setTimeout(() => {
+  process.stdout.write(char)
+}, counter += 50)
+}
+console.log();
+
